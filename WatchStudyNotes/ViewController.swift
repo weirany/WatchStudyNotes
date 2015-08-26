4dd3f259b353cdb18c15b5ca14a5f20d7c9c17c3
Weiran Ye <williamwry@gmail.com>
2015-08-25 19:22:02 -0700

    Initial checkin.

---
+++ WatchStudyNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchStudyNotes
+//
+//  Created by Weiran Ye on 8/25/15.
+//  Copyright (c) 2015 Weiran Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
